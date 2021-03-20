48f2bd61eee67118d52c91f596a06eb9026df541
Lizandro1 <lizandroaquino31@gmail.com>
2021-03-20 16:36:45 -0300

    Começo do Menu

---
+++ PoisonousGame/Assets/Scripts/Models/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string nomeDaCena;
+     
+    public void mudarCena()
+    {
+        SceneManager.LoadScene(nomeDaCena);    
+    
+    }
+
+    public void sair()
+    {
+        Application.Quit();
+	}
+}

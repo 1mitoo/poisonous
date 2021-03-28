9ccd2509b61db8a0a0858078464c92f43ab146f1
Lizandro1 <lizandroaquino31@gmail.com>
2021-03-28 20:41:13 -0300

    Sistema de dialógo(para aparecer a mensagem)

---
+++ PoisonousGame/Assets/Scripts/Models/Dialogue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+	public Sprite profile;
+	[TextArea(1,4)]
+	public string [] texto;
+	public string nomeNpc;
+	
+	public LayerMask playerLayer;
+	public float radious;
+
+	private DialogueControl dialogo;
+	private bool areaDialogo;
+	public void Start()
+	{
+		dialogo = FindObjectOfType<DialogueControl>();
+	}
+
+	private void FixedUpdate()
+	{
+		Interact();
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space) && areaDialogo)
+		{
+			dialogo.Speech(profile, texto, nomeNpc);
+		}
+	}
+	public void Interact()
+	{
+		Collider2D x = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
+
+		if(x != null)
+		{
+			areaDialogo = true;
+		}
+		else
+		{
+			areaDialogo = false;
+		}
+	}
+
+	 private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radious);
+	}
+}
---
+++ PoisonousGame/Assets/Scripts/Models/DialogueControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject dialogue;
+    public Image profile;
+    public TextMeshProUGUI texto;
+    public TextMeshProUGUI nomeNpc;
+
+    [Header("Settings")]
+    public float velocidadeTexto;
+    private string[] frases;
+    private int index;
+
+    public void Speech(Sprite p, string [] text, string nome)
+    {
+        dialogue.SetActive(true);  
+        profile.sprite = p;
+        frases = text;
+        nomeNpc.text = nome;
+        StartCoroutine(TypeSentence());
+	}
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letras in frases[index].ToCharArray())
+        {
+            texto.text += letras;
+            yield return new WaitForSeconds(velocidadeTexto);
+		}
+	}
+    
+    public void ProximaFrase()
+    {
+        if(texto.text == frases[index])
+        {
+            if(index < frases.Length - 1)
+            {
+                index++; 
+                texto.text = "";
+                StartCoroutine(TypeSentence());
+			}
+            else
+            {
+                texto.text = "";
+                index = 0;
+                dialogue.SetActive(false);  
+			}
+		}
+	}
+}
